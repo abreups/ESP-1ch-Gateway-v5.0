e6ac92e0de51bfdccf3ff3a74c5db335eaf25df5
Paulo S. Abreu <abreups@gmail.com>
2018-06-05 10:50:55 -0300

    embelezamento do código

--- ESP-sc-gway/loraModem.h
+++ ESP-sc-gway/loraModem.h
@@ -212,94 +212,93 @@ struct LoraUp {
 // Register definitions. These are the addresses of the TFM95, SX1276 that we 
 // need to set in the program.
 
-#define REG_FIFO                    0x00
-#define REG_OPMODE                  0x01
+#define REG_FIFO			0x00
+#define REG_OPMODE			0x01
 // Register 2 to 5 are unused for LoRa
-#define REG_FRF_MSB					        0x06
-#define REG_FRF_MID					        0x07
-#define REG_FRF_LSB					        0x08
-#define REG_PAC                     0x09
-#define REG_PARAMP                  0x0A
-#define REG_LNA                     0x0C
-#define REG_FIFO_ADDR_PTR           0x0D
-#define REG_FIFO_TX_BASE_AD         0x0E
-#define REG_FIFO_RX_BASE_AD         0x0F
+#define REG_FRF_MSB			0x06
+#define REG_FRF_MID			0x07
+#define REG_FRF_LSB			0x08
+#define REG_PAC				0x09
+#define REG_PARAMP			0x0A
+#define REG_LNA				0x0C
+#define REG_FIFO_ADDR_PTR		0x0D
+#define REG_FIFO_TX_BASE_AD		0x0E
+#define REG_FIFO_RX_BASE_AD		0x0F
 
-#define REG_FIFO_RX_CURRENT_ADDR    0x10
-#define REG_IRQ_FLAGS_MASK          0x11
-#define REG_IRQ_FLAGS               0x12
-#define REG_RX_NB_BYTES             0x13
-#define REG_PKT_SNR_VALUE			      0x19
-#define REG_PKT_RSSI				        0x1A	// latest package
-#define REG_RSSI					          0x1B	// Current RSSI, section 6.4, or  5.5.5
-#define REG_HOP_CHANNEL				      0x1C
-#define REG_MODEM_CONFIG1           0x1D
-#define REG_MODEM_CONFIG2           0x1E
-#define REG_SYMB_TIMEOUT_LSB  		  0x1F
+#define REG_FIFO_RX_CURRENT_ADDR	0x10
+#define REG_IRQ_FLAGS_MASK		0x11
+#define REG_IRQ_FLAGS			0x12
+#define REG_RX_NB_BYTES			0x13
+#define REG_PKT_SNR_VALUE		0x19
+#define REG_PKT_RSSI			0x1A	// latest package
+#define REG_RSSI			0x1B	// Current RSSI, section 6.4, or  5.5.5
+#define REG_HOP_CHANNEL			0x1C
+#define REG_MODEM_CONFIG1		0x1D
+#define REG_MODEM_CONFIG2		0x1E
+#define REG_SYMB_TIMEOUT_LSB		0x1F
 
-#define REG_PAYLOAD_LENGTH          0x22
-#define REG_MAX_PAYLOAD_LENGTH 		  0x23
-#define REG_HOP_PERIOD              0x24
-#define REG_MODEM_CONFIG3           0x26
-#define REG_RSSI_WIDEBAND			      0x2C
+#define REG_PAYLOAD_LENGTH		0x22
+#define REG_MAX_PAYLOAD_LENGTH		0x23
+#define REG_HOP_PERIOD			0x24
+#define REG_MODEM_CONFIG3		0x26
+#define REG_RSSI_WIDEBAND		0x2C
 
-#define REG_INVERTIQ				        0x33
-#define REG_DET_TRESH				        0x37	// SF6
-#define REG_SYNC_WORD				        0x39
-#define REG_TEMP					          0x3C
+#define REG_INVERTIQ			0x33
+#define REG_DET_TRESH			0x37	// SF6
+#define REG_SYNC_WORD			0x39
+#define REG_TEMP			0x3C
 
-#define REG_DIO_MAPPING_1           0x40
-#define REG_DIO_MAPPING_2           0x41
-#define REG_VERSION	  				      0x42
+#define REG_DIO_MAPPING_1		0x40
+#define REG_DIO_MAPPING_2		0x41
+#define REG_VERSION			0x42
 
-#define REG_PADAC					          0x5A
-#define REG_PADAC_SX1272			      0x5A
-#define REG_PADAC_SX1276			      0x4D
+#define REG_PADAC			0x5A
+#define REG_PADAC_SX1272		0x5A
+#define REG_PADAC_SX1276		0x4D
 
 
 // ----------------------------------------
 // opModes
-#define SX72_MODE_SLEEP             0x80
-#define SX72_MODE_STANDBY           0x81
-#define SX72_MODE_FSTX              0x82
-#define SX72_MODE_TX                0x83	// 0x80 | 0x03
-#define SX72_MODE_RX_CONTINUOS      0x85
+#define SX72_MODE_SLEEP			0x80
+#define SX72_MODE_STANDBY		0x81
+#define SX72_MODE_FSTX			0x82
+#define SX72_MODE_TX			0x83	// 0x80 | 0x03
+#define SX72_MODE_RX_CONTINUOS		0x85
 
 // ----------------------------------------
 // LMIC Constants for radio registers
-#define OPMODE_LORA      			      0x80
-#define OPMODE_MASK      			      0x07
-#define OPMODE_SLEEP     			      0x00
-#define OPMODE_STANDBY   			      0x01
-#define OPMODE_FSTX      			      0x02
-#define OPMODE_TX        			      0x03
-#define OPMODE_FSRX      			      0x04
-#define OPMODE_RX        			      0x05
-#define OPMODE_RX_SINGLE 			      0x06
-#define OPMODE_CAD       			      0x07
+#define OPMODE_LORA			0x80
+#define OPMODE_MASK			0x07
+#define OPMODE_SLEEP			0x00
+#define OPMODE_STANDBY			0x01
+#define OPMODE_FSTX			0x02
+#define OPMODE_TX			0x03
+#define OPMODE_FSRX			0x04
+#define OPMODE_RX			0x05
+#define OPMODE_RX_SINGLE		0x06
+#define OPMODE_CAD			0x07
 
 
 
 // ----------------------------------------
 // LOW NOISE AMPLIFIER
-
-#define LNA_MAX_GAIN                0x23
-#define LNA_OFF_GAIN                0x00
-#define LNA_LOW_GAIN		    	0x20
+#define LNA_MAX_GAIN			0x23
+#define LNA_OFF_GAIN			0x00
+#define LNA_LOW_GAIN			0x20
 
 // CONF REG
-#define REG1                        0x0A
-#define REG2                        0x84
+#define REG1				0x0A
+#define REG2				0x84
 
 // ----------------------------------------
 // MC1 sx1276 RegModemConfig1
-#define SX1276_MC1_BW_125           0x70
-#define SX1276_MC1_BW_250           0x80
-#define SX1276_MC1_BW_500           0x90
-#define SX1276_MC1_CR_4_5           0x02
-#define SX1276_MC1_CR_4_6           0x04
-#define SX1276_MC1_CR_4_7           0x06
-#define SX1276_MC1_CR_4_8           0x08
+#define SX1276_MC1_BW_125		0x70
+#define SX1276_MC1_BW_250		0x80
+#define SX1276_MC1_BW_500		0x90
+#define SX1276_MC1_CR_4_5		0x02
+#define SX1276_MC1_CR_4_6		0x04
+#define SX1276_MC1_CR_4_7		0x06
+#define SX1276_MC1_CR_4_8		0x08
 #define SX1276_MC1_IMPLICIT_HEADER_MODE_ON  0x01
 
 #define SX72_MC1_LOW_DATA_RATE_OPTIMIZE     0x01 	// mandated for SF11 and SF12
@@ -364,15 +363,15 @@ struct LoraUp {
 
 // ----------------------------------------
 // Definitions for UDP message arriving from server
-#define PROTOCOL_VERSION			0x01
-#define PKT_PUSH_DATA				0x00
-#define PKT_PUSH_ACK				0x01
-#define PKT_PULL_DATA				0x02
-#define PKT_PULL_RESP				0x03
-#define PKT_PULL_ACK				0x04
-#define PKT_TX_ACK                  0x05
+#define PROTOCOL_VERSION		0x01
+#define PKT_PUSH_DATA			0x00
+#define PKT_PUSH_ACK			0x01
+#define PKT_PULL_DATA			0x02
+#define PKT_PULL_RESP			0x03
+#define PKT_PULL_ACK			0x04
+#define PKT_TX_ACK                  	0x05
 
-#define MGT_RESET					0x15	// Not a LoRa Gateway Spec message
-#define MGT_SET_SF					0x16
-#define MGT_SET_FREQ				0x17
+#define MGT_RESET			0x15	// Not a LoRa Gateway Spec message
+#define MGT_SET_SF			0x16
+#define MGT_SET_FREQ			0x17
 
